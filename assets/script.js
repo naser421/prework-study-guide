af1d833d8384806291ccc3ea5cca54fa08ee2437
Naser Suleiman <nasersuleiman@Nasers-MacBook-Pro.local>
2022-09-14 10:12:36 -0700

    mahmed4

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,13 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-/*
+
+function listTopics() {
+  for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+  }
+}
+for ( var x=0 ; x<topics.length; x++) {
+  console.log(topics[x]);
+}
 if (topic === 'HTML') {
   console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -11,8 +19,4 @@ if (topic === 'HTML') {
 } else {
   console.log('Please try again!');
 }
-*/
-for ( var x=0 ; x<topics.length; x++) {
-  console.log(topics[x]);
 
-}
